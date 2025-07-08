e9f626be0be014e2bbb4ca3219107320b3633241
trungnotchung <trung09072004@gmail.com>
2025-07-08 10:12:30 +0700

    feat: generate migration

---
+++ src/migrations/1751944222239-migrations.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migrations1751944222239 implements MigrationInterface {
+    name = 'Migrations1751944222239'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`ephemeral_keys\` (\`ephId\` varchar(36) NOT NULL, \`ephemeralKey\` varchar(64) NOT NULL, \`txHash\` varchar(66) NOT NULL, \`blockHeight\` bigint NOT NULL, \`seenAt\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`stealth_id\` varchar(36) NULL, PRIMARY KEY (\`ephId\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`transactions\` (\`txId\` varchar(36) NOT NULL, \`txHash\` varchar(66) NOT NULL, \`timestamp\` timestamp NOT NULL, \`direction\` enum ('IN', 'OUT') NOT NULL, \`assetType\` enum ('coin', 'ft', 'nft') NOT NULL, \`tokenAddress\` text NULL, \`tokenId\` text NULL, \`amount\` decimal NULL, \`stealth_id\` varchar(36) NULL, PRIMARY KEY (\`txId\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`stealth_addresses\` (\`stealthId\` varchar(36) NOT NULL, \`address\` varchar(64) NOT NULL, \`viewTag\` smallint NULL, \`createdAt\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`meta_id\` varchar(36) NULL, UNIQUE INDEX \`IDX_4efd957e1ab5de431437520fc3\` (\`address\`), PRIMARY KEY (\`stealthId\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`meta_addresses\` (\`metaId\` varchar(36) NOT NULL, \`scanPublic\` varchar(64) NOT NULL, \`spendPublic\` varchar(64) NOT NULL, \`scanPrivateEncrypted\` text NOT NULL, \`spendPrivateEncrypted\` text NOT NULL, \`createdAt\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`user_id\` varchar(36) NULL, PRIMARY KEY (\`metaId\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`users\` (\`userId\` varchar(36) NOT NULL, \`username\` varchar(255) NOT NULL, \`aptosPublicKey\` varchar(64) NOT NULL, \`createdAt\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), UNIQUE INDEX \`IDX_fe0bb3f6520ee0469504521e71\` (\`username\`), PRIMARY KEY (\`userId\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`ALTER TABLE \`ephemeral_keys\` ADD CONSTRAINT \`FK_8c5fb5457c8bb6c4f780570f9b7\` FOREIGN KEY (\`stealth_id\`) REFERENCES \`stealth_addresses\`(\`stealthId\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE \`transactions\` ADD CONSTRAINT \`FK_ed1b249147b7235a12d90bb3c34\` FOREIGN KEY (\`stealth_id\`) REFERENCES \`stealth_addresses\`(\`stealthId\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE \`stealth_addresses\` ADD CONSTRAINT \`FK_5102c89a44bcdb2f5761883fce8\` FOREIGN KEY (\`meta_id\`) REFERENCES \`meta_addresses\`(\`metaId\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE \`meta_addresses\` ADD CONSTRAINT \`FK_907705c14de59804fbcd708d396\` FOREIGN KEY (\`user_id\`) REFERENCES \`users\`(\`userId\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`meta_addresses\` DROP FOREIGN KEY \`FK_907705c14de59804fbcd708d396\``);
+        await queryRunner.query(`ALTER TABLE \`stealth_addresses\` DROP FOREIGN KEY \`FK_5102c89a44bcdb2f5761883fce8\``);
+        await queryRunner.query(`ALTER TABLE \`transactions\` DROP FOREIGN KEY \`FK_ed1b249147b7235a12d90bb3c34\``);
+        await queryRunner.query(`ALTER TABLE \`ephemeral_keys\` DROP FOREIGN KEY \`FK_8c5fb5457c8bb6c4f780570f9b7\``);
+        await queryRunner.query(`DROP INDEX \`IDX_fe0bb3f6520ee0469504521e71\` ON \`users\``);
+        await queryRunner.query(`DROP TABLE \`users\``);
+        await queryRunner.query(`DROP TABLE \`meta_addresses\``);
+        await queryRunner.query(`DROP INDEX \`IDX_4efd957e1ab5de431437520fc3\` ON \`stealth_addresses\``);
+        await queryRunner.query(`DROP TABLE \`stealth_addresses\``);
+        await queryRunner.query(`DROP TABLE \`transactions\``);
+        await queryRunner.query(`DROP TABLE \`ephemeral_keys\``);
+    }
+
+}
