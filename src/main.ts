dafc3e2698e51bbb97913423b2e6d202bf58d4ec
trungnotchung <trung09072004@gmail.com>
2025-07-08 10:55:58 +0700

    refactor: remove global aptos middleware

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { json, urlencoded } from 'express';
 
 import { AppModule } from './app.module.js';
-import { AptosSignatureMiddleware } from './middleware/aptos-signature.middleware.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,9 +13,6 @@ async function bootstrap() {
   });
   app.use(json({ limit: '100mb' }));
   app.use(urlencoded({ extended: true, limit: '100mb' }));
-
-  const aptosMiddleware = new AptosSignatureMiddleware();
-  app.use(aptosMiddleware.use.bind(aptosMiddleware));
 
   await app.listen(process.env.PORT ?? 3000);
 }
