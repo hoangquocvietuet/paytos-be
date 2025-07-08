784439738599ba538b250180473a4f5300658c2b
trungnotchung <trung09072004@gmail.com>
2025-07-08 10:12:03 +0700

    feat: update load config

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,26 +9,9 @@ function loadConfig() {
       port: parseInt(process.env.PORT || '3000', 10),
     },
     database: {
-      url: process.env.DATABASE_URL || 'mysql://root:root@localhost:3306/u4s',
+      url:
+        process.env.DATABASE_URL || 'mysql://root:root@localhost:3307/paytos',
     },
-    auth: {
-      jwt: {
-        secret: process.env.JWT_SECRET || 'default-secret-key-for-development',
-      },
-      encryption: {
-        key: process.env.ENCRYPTION_KEY,
-      },
-    },
-    redis: {
-      url: process.env.REDIS_URL || 'redis://localhost:6379',
-    },
-    pinata: {
-      pinataJwt: process.env.PINATA_JWT,
-    },
-    chain: {
-      rpc_url: process.env.RPC_URL || '',
-    },
-    private_key: process.env.PRIVATE_KEY || '',
   };
 
   return config;
